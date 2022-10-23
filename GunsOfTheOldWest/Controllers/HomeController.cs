ae895489362cdfda9713fb2e7c4510561a9f08db
David Vandersteene <david.vandersteene@student.vives.be>
2022-10-23 13:32:42 +0200

    Made gun controller Made index page look like asked Made WinnaarScherm

--- GunsOfTheOldWest/Controllers/HomeController.cs
+++ GunsOfTheOldWest/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace GunsOfTheOldWest.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private GunsFromTheOldWestModel gunModel = new GunsFromTheOldWestModel(12);
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -18,9 +19,21 @@ namespace GunsOfTheOldWest.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult WinnaarScherm()
         {
             return View();
+        }
+
+        public IActionResult Shoot([FromRoute] int bullets)
+        {
+            if (bullets <= 0)
+            {
+                return WinnaarScherm();
+            }
+            else
+            {
+                
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ GunsOfTheOldWest/Models/GunsFromTheOldWestModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GunsOfTheOldWest.Models
+{
+    public class GunsFromTheOldWestModel
+    {
+        public GunsFromTheOldWestModel(int bullets)
+        {
+            this.bullets = bullets;
+        }
+        public int bullets { get; set; }
+    }
+}
